1fb699fb387746c616f8a9fa7c2fb825cc1205e2
Iulian Popa <bobel2007@outlook.com>
2021-01-27 14:31:00 +0200

    Add form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './Components/layout/Footer';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Docs from './Components/Docs';
+import PieForm from './Components/PieForm';
 
 function App() {
 	return (
@@ -13,6 +14,7 @@ function App() {
 				<div className='main-container'>
 					<Route exact path='/' component={Home}></Route>
 					<Route exact path='/docs' component={Docs}></Route>
+					<Route path="/create" component={PieForm}></Route>
 				</div>
 
 				<Footer />
