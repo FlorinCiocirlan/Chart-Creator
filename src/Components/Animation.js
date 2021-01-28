a5b21e0ccaee4af300546b095d99ac477e896080
Iulian Popa <55912104+bobel95@users.noreply.github.com>
2021-01-28 19:45:39 +0200

    Update Animation.js

--- src/Components/Animation.js
+++ src/Components/Animation.js
@@ -1,30 +1,10 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import 'fullpage.js/vendors/scrolloverflow'; // Optional. When using scrollOverflow:true
-import fifthImage from '../fifthImage.png';
-import '../Animation.css';
-import ButtonPage from './GetStartedButton';
-=======
 import React from "react";
 import "fullpage.js/vendors/scrolloverflow"; // Optional. When using scrollOverflow:true
 import fifthImage from "../fifthImage.png";
 import "../Animation.css";
 import ButtonPage from "./GetStartedButton";
->>>>>>> Stashed changes
-
-// import './styles.css';
 
 class FullpageWrapper extends React.Component {
-<<<<<<< Updated upstream
-	render() {
-		return (
-			<div className='home-div'>
-				<img alt="people who use chart app" src={fifthImage} fluid className='home-image' />
-				<ButtonPage />
-			</div>
-		);
-	}
-=======
   render() {
     return (
       <div className="home-div">
@@ -34,12 +14,11 @@ class FullpageWrapper extends React.Component {
             online
           </h1>
         </span>
-        <img src={fifthImage} fluid className="home-image" />
+        <img src={fifthImage} alt="home-image" fluid className="home-image" />
         <ButtonPage />
       </div>
     );
   }
->>>>>>> Stashed changes
 }
 
 export default FullpageWrapper;
